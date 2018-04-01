ca67602cdc757e74dcd991adde771f2b2656ffa0
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-01 17:05:28 +0300

    Remove api.js from index

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import WebFont from "webfontloader";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-import WebFont from "webfontloader";
-
-import api from "./api";
+import "./index.css";
 
 WebFont.load({
   google: {
