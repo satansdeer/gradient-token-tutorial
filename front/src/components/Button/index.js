a59e1d543965b648c5a74c9371e314b797d6e64a
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-02 08:03:18 +0300

    Add proptypes to button

--- front/src/components/Button/index.js
+++ front/src/components/Button/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Button.css";
 
-export default ({ onClick, label, disabled }) => (
+const Button = ({ onClick, label, disabled }) => (
   <button onClick={onClick} className={`Button ${disabled && "is-disabled"}`}>
     {label}
   </button>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  label: PropTypes.string,
+  disabled: PropTypes.bool
+};
+
+export default Button;
