ac8e219ba44119e97e469baa1c30098b71f44faf
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-02 06:22:22 +0300

    Add empty state for TokensList

--- front/src/TokensPage/TokensList/index.js
+++ front/src/TokensPage/TokensList/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import TokenItem from "./TokenItem";
 import TokenView from "./TokenView";
 import { inject, observer } from "mobx-react";
@@ -7,15 +7,23 @@ import "./TokensList.css";
 export default inject("gradientTokenStore", "modalStore")(
   observer(({ gradientTokenStore: { tokens }, modalStore }) => {
     return (
-      <div className="TokensList">
-        {tokens.map((token, index) => (
-          <TokenItem
-            key={index}
-            token={token}
-            onClick={() => modalStore.showModal(<TokenView token={token} />)}
-          />
-        ))}
-      </div>
+      <Fragment>
+        {tokens.length ? (
+          <div className="TokensList">
+            {tokens.map((token, index) => (
+              <TokenItem
+                key={index}
+                token={token}
+                onClick={() =>
+                  modalStore.showModal(<TokenView token={token} />)
+                }
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="TokensList-label_empty">You don't have tokens yet.</div>
+        )}
+      </Fragment>
     );
   })
 );
