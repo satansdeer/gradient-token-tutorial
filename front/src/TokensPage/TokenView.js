6ff1370d9a65680962b566e9e6b3b767968d0ae9
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-02 06:07:16 +0300

    Move TokenImage to components

--- front/src/TokensPage/TokenView.js
+++ front/src/TokensPage/TokenView.js
@@ -1,11 +1,11 @@
 import React from "react";
-import TokenItemImage from "./TokenItemImage.jsx";
+import TokenImage from "components/TokenImage";
 import "./TokenView.css";
 
 export default ({ token }) => (
   <div>
     <div className="TokenView-image_wrapper">
-      <TokenItemImage size={200} outer={token[0]} inner={token[1]} />
+      <TokenImage size={200} outer={token[0]} inner={token[1]} />
     </div>
     <div className="TokenView-label">{`${token[0]} – ${token[1]}`}</div>
   </div>
--- front/src/TokensPage/TokensList/TokenItem/index.js
+++ front/src/TokensPage/TokensList/TokenItem/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import TokenItemImage from "./TokenItemImage.jsx";
+import TokenImage from "components/TokenImage";
 import "./TokenItem.css";
 
 export default ({ token, onClick }) => {
   return (
     <div className="TokenItem" onClick={onClick}>
       <div className="TokenItem-image_wrapper">
-        <TokenItemImage outer={token[0]} inner={token[1]} />
+        <TokenImage outer={token[0]} inner={token[1]} />
       </div>
       <div className="TokenItem-label">{`${token[0]} – ${token[1]}`}</div>
     </div>
